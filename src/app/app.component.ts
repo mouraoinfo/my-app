bc36630df412c9d9509d21f98922d27fab13d206
mouraoinfo <marcos@mourao.info>
2017-09-27 00:52:14 -0300

    primeiro commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-raiz',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  titulo = 'Meu primeiro App Angular';
+  nome = 'Mari Caramello';
+}
